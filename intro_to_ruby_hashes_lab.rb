67589bde7670f7ac0e80d25ae5ac8ee7f9ed5f0a
declarative-blog-8005 <anna@annajcarey.com>
2019-10-31 19:38:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,11 +25,12 @@ def monopoly_with_third_tier
     :railroads =>{
       :pieces => 4, 
       :rent_in_dollars => {
-        :names => 1, 
+        :one => 1, 
         :two => 2,
         :three => 3,
         :four => 4
-      }
+      }, 
+      :names => 1
     }
   }
 end
